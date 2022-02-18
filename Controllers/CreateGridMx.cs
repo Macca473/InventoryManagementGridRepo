368c811863a337292eeeef88b4af6baeed8d58d3
Matthew <mmacca473@gmail.com>
2022-02-18 13:05:16 +1030

    added constructer

--- Controllers/CreateGridMx.cs
+++ Controllers/CreateGridMx.cs
@@ -12,25 +12,36 @@ namespace InventoryManagementGrid.Controllers
         {
             public int width { get; set; }
             public int depth { get; set; }
-            public bool isgoal { get; set; }
-            public bool isfilled { get; set; }
-            public int goaldis { get; set; }
+            public bool isgoal { get; set; } = false;
+            public bool isfilled { get; set; } = false;
+            public int goaldis { get; set; } = 0;
+
+            public Sq(int w, int d)
+            {
+                width = w;
+                depth = d;
+            }
         }
 
-        static List<string> MakeGrid(int width, int depth)
+        static List<Sq> MakeGrid(int width, int depth)
         {
-            List<string> GridLocs = new List<string>();
+            List<Sq> GridLocs = new List<Sq>();
 
             for (int wit = 0; wit < width; wit++)
             {
-
                 for (int dep = 0; dep < width; dep++)
                 {
-                    GridLocs.Add("W" + wit + ":D" + dep);
+                    Sq thissq = new Sq(wit, dep);
+
+                    GridLocs.Add(thissq);
+
+                    //Console.WriteLine(thissq.depth + " " + thissq.width);
                 }
             }
             return GridLocs;
         }
+
+
     }
 
     
