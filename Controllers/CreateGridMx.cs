254359fe714d9cd2a6fb81a26ee541481a6dccf3
Matthew <mmacca473@gmail.com>
2022-03-01 16:51:51 +1030

    Almost Finished

--- Controllers/CreateGridMx.cs
+++ Controllers/CreateGridMx.cs
@@ -27,48 +27,6 @@ namespace InventoryManagementGrid.Controllers
 
         public static List<List<Sq>> SqList { get; set; }
 
-        static void Inptwn(Sq S1, Sq S2)
-        {
-            Sq twn1 = new Sq(S1.width, S2.depth);
-
-            Sq twn2 = new Sq(S2.width, S1.depth);
-
-            //Console.WriteLine("Input Num1: W" + S1.width + "D:" + S1.depth + " Input Num1: W" + S2.width + "D:" + S2.depth);
-            //Console.WriteLine("Output Num1: W" + twn1.width + "D:" + twn1.depth + " Output Num1: W" + twn2.width + "D:" + twn2.depth);
-
-            //List<Sq> Inpt = new List<Sq>();
-
-            Console.WriteLine(SqList.Count);
-
-            //List<Sq> Inpt = Inb(S1, twn1);
-
-            List<Sq> Inpt = Inb(twn1, S1);
-
-            for (int inx = 0; inx < Inpt.Count; inx++)
-            {
-                Console.WriteLine(Inpt[inx].width + " | " + Inpt[inx].depth);
-            }
-        }
-
-        static List<Sq> MakeGrid(int width, int depth)
-        {
-            List<Sq> GridLocs = new List<Sq>();
-
-            for (int wit = 0; wit < width; wit++)
-            {
-                for (int dep = 0; dep < width; dep++)
-                {
-                    Sq thissq = new Sq(wit, dep);
-
-                    GridLocs.Add(thissq);
-
-                    //Console.WriteLine(thissq.depth + " " + thissq.width);
-                }
-            }
-            return GridLocs;
-        }
-
-
     }
 
     
